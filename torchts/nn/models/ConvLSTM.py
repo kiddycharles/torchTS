4dea958d2d36654c626556211e0d626cfc61085f
kiddycharles <juy022@ucsd.edu>
2024-05-15 03:03:37 -0700

    Implement Seq2SeqConvLSTM module for spatiotemporal data processing Add ConvLSTM module for efficient processing of spatiotemporal data. This module combines the capabilities of convolutional layers and LSTM units, enabling the model to capture both spatial and temporal dependencies in sequences effectively. This enhances the model's ability to understand and predict complex patterns in video, sensor data, and other spatiotemporal data types.

--- torchts/nn/models/ConvLSTM.py
+++ torchts/nn/models/ConvLSTM.py
@@ -53,3 +53,98 @@ class ConvLSTMCell(nn.Module):
         height, width = image_size
         return (torch.zeros(batch_size, self.hidden_dim, height, width, device=self.conv.weight.device),
                 torch.zeros(batch_size, self.hidden_dim, height, width, device=self.conv.weight.device))
+
+
+class Seq2SeqConvLSTM(nn.Module):
+    def __init__(self, nf, in_channel):
+        super(Seq2SeqConvLSTM, self).__init__()
+
+        """ 
+        Overall Architecture
+        - Encoder
+            # ConvLSTM
+            # Hidden Representation from the Encoder
+        - Decoder
+            # ConvLSTM (Takes in Hidden Representation from the Encoder)
+            # 3D CNN  (Produces Regression Predictions for Our Model)
+        """
+
+        self.encoder_layer_1_convlstm = ConvLSTMCell(input_dim=in_channel,
+                                                     hidden_dim=nf,
+                                                     kernel_size=(3, 3),
+                                                     bias=True)
+
+        self.encoder_layer_2_convlstm = ConvLSTMCell(input_dim=nf,
+                                                     hidden_dim=nf,
+                                                     kernel_size=(3, 3),
+                                                     bias=True)
+
+        self.decoder_layer_1_convlstm = ConvLSTMCell(input_dim=nf,
+                                                     hidden_dim=nf,
+                                                     kernel_size=(3, 3),
+                                                     bias=True)
+
+        self.decoder_layer_2_convlstm = ConvLSTMCell(input_dim=nf,
+                                                     hidden_dim=nf,
+                                                     kernel_size=(3, 3),
+                                                     bias=True)
+
+        self.decoder_CNN = nn.Conv3d(in_channels=nf,
+                                     out_channels=1,
+                                     kernel_size=(1, 3, 3),
+                                     padding=(0, 1, 1))
+
+    def autoencoder(self, x, seq_len, future_step, h_t, c_t, h_t2, c_t2, h_t3, c_t3, h_t4, c_t4):
+
+        outputs = []
+
+        # encoder
+        for t in range(seq_len):
+            h_t, c_t = self.encoder_1_convlstm(input_tensor=x[:, t, :, :],
+                                               cur_state=[h_t, c_t])  # we could concat to provide skip conn here
+            h_t2, c_t2 = self.encoder_2_convlstm(input_tensor=h_t,
+                                                 cur_state=[h_t2, c_t2])  # we could concat to provide skip conn here
+
+        # encoder_vector
+        encoder_vector = h_t2
+
+        # decoder
+        for t in range(future_step):
+            h_t3, c_t3 = self.decoder_1_convlstm(input_tensor=encoder_vector,
+                                                 cur_state=[h_t3, c_t3])  # we could concat to provide skip conn here
+            h_t4, c_t4 = self.decoder_2_convlstm(input_tensor=h_t3,
+                                                 cur_state=[h_t4, c_t4])  # we could concat to provide skip conn here
+            encoder_vector = h_t4
+            outputs += [h_t4]  # predictions
+
+        outputs = torch.stack(outputs, 1)
+        outputs = outputs.permute(0, 2, 1, 3, 4)
+        outputs = self.decoder_CNN(outputs)
+        outputs = torch.nn.Sigmoid()(outputs)
+
+        return outputs
+
+    def forward(self, x, future_seq=0):
+
+        """
+        Parameters
+        ----------
+        input_tensor:
+            5-D Tensor of shape (b, t, c, h, w)        #   batch, time, channel, height, width
+            :param future_seq:
+            :param x:
+        """
+
+        # find size of different input dimensions
+        b, seq_len, _, h, w = x.size()
+
+        # initialize hidden states
+        h_t, c_t = self.encoder_1_convlstm.init_hidden(batch_size=b, image_size=(h, w))
+        h_t2, c_t2 = self.encoder_2_convlstm.init_hidden(batch_size=b, image_size=(h, w))
+        h_t3, c_t3 = self.decoder_1_convlstm.init_hidden(batch_size=b, image_size=(h, w))
+        h_t4, c_t4 = self.decoder_2_convlstm.init_hidden(batch_size=b, image_size=(h, w))
+
+        # autoencoder forward
+        outputs = self.autoencoder(x, seq_len, future_seq, h_t, c_t, h_t2, c_t2, h_t3, c_t3, h_t4, c_t4)
+
+        return outputs
