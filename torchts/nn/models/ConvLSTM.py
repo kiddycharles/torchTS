da2baac5b0ddd0e377c7775def355a6eac5a1876
kiddycharles <juy022@ucsd.edu>
2024-05-27 23:59:07 -0700

    Title: Add Conv2dActivation and make_layers functions
    Description:
    Added the Conv2dActivation function for creating convolutional and deconvolutional layers with ReLU or Leaky ReLU activation functions based on the layer name. Also implemented the make_layers function to construct a Sequential container of layers based on the provided block configuration. This commit enhances modularity and flexibility in defining neural network architectures.

--- torchts/nn/models/ConvLSTM.py
+++ torchts/nn/models/ConvLSTM.py
@@ -1,222 +1,125 @@
+from typing import Tuple, Union
+from enum import Enum
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
-from torchts.utils.make_layers import make_layers
-from torchts.nn.model import TimeSeriesModel
-import logging
+from torch import nn
+
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
 
-class ConvGRU_cell(nn.Module):
-    """
-    ConvGRU Cell
-    """
+class WeightsInitializer(str, Enum):
+    Zeros = "zeros"
+    He = "he"
+    Xavier = "xavier"
 
-    def __init__(self, shape, input_channels, filter_size, num_features):
-        super(ConvGRU_cell, self).__init__()
-        self.shape = shape
-        self.input_channels = input_channels
-        # kernel_size of input_to_state equals state_to_state
-        self.filter_size = filter_size
-        self.num_features = num_features
-        self.padding = (filter_size - 1) // 2
-        self.conv1 = nn.Sequential(
-            nn.Conv2d(self.input_channels + self.num_features,
-                      2 * self.num_features, self.filter_size, 1,
-                      self.padding),
-            nn.GroupNorm(2 * self.num_features // 32, 2 * self.num_features))
-        self.conv2 = nn.Sequential(
-            nn.Conv2d(self.input_channels + self.num_features,
-                      self.num_features, self.filter_size, 1, self.padding),
-            nn.GroupNorm(self.num_features // 32, self.num_features))
 
-    def forward(self, inputs=None, hidden_state=None, seq_len=10):
-        # seq_len=10 for moving_mnist
-        if hidden_state is None:
-            htprev = torch.zeros(inputs.size(1), self.num_features,
-                                 self.shape[0], self.shape[1]).cuda()
+class BaseConvLSTMCell(nn.Module):
+    """The ConvLSTM Cell implementation."""
+
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        kernel_size: Union[int, Tuple],
+        padding: Union[int, Tuple, str],
+        activation: str,
+        frame_size: Tuple,
+        weights_initializer: WeightsInitializer = WeightsInitializer.Zeros,
+    ) -> None:
+        """
+
+        Args:
+            in_channels (int): Number of channels of input tensor.
+            out_channels (int): Number of channels of output tensor
+            kernel_size (Union[int, Tuple]): Size of the convolution kernel.
+            padding (padding (Union[int, Tuple, str]): 'same', 'valid' or (int, int)
+            activation (str): Name of activation function
+            frame_size (Tuple): height and width
+        """
+        super().__init__()
+        self.activation = self.__activation(activation)
+        self.conv = nn.Conv2d(
+            in_channels=in_channels + out_channels,
+            out_channels=4 * out_channels,
+            kernel_size=kernel_size,
+            padding=padding,
+            device=DEVICE,
+        )
+
+        self.W_ci = nn.parameter.Parameter(
+            torch.zeros(out_channels, *frame_size, dtype=torch.float)
+        ).to(DEVICE)
+        self.W_co = nn.parameter.Parameter(
+            torch.zeros(out_channels, *frame_size, dtype=torch.float)
+        ).to(DEVICE)
+        self.W_cf = nn.parameter.Parameter(
+            torch.zeros(out_channels, *frame_size, dtype=torch.float)
+        ).to(DEVICE)
+        self.__initialize_weights(weights_initializer)
+
+    def __activation(self, activation: str) -> nn.Module:
+        if activation == "tanh":
+            return nn.Tanh()
+        elif activation == "relu":
+            return nn.ReLU()
+        elif activation == "leakyRelu":
+            return nn.LeakyReLU()
+        elif activation == "sigmoid":
+            return nn.Sigmoid()
         else:
-            htprev = hidden_state
-        output_inner = []
-        for index in range(seq_len):
-            if inputs is None:
-                x = torch.zeros(htprev.size(0), self.input_channels,
-                                self.shape[0], self.shape[1]).cuda()
-            else:
-                x = inputs[index, ...]
+            raise ValueError(f"Unknown activation: {activation}")
 
-            combined_1 = torch.cat((x, htprev), 1)  # X_t + H_t-1
-            gates = self.conv1(combined_1)  # W * (X_t + H_t-1)
+    def __initialize_weights(self, initializer: WeightsInitializer):
+        if initializer == WeightsInitializer.Zeros:
+            return
 
-            zgate, rgate = torch.split(gates, self.num_features, dim=1)
-            # zgate, rgate = gates.chunk(2, 1)
-            z = torch.sigmoid(zgate)
-            r = torch.sigmoid(rgate)
+        elif initializer == WeightsInitializer.He:
+            nn.init.kaiming_normal_(self.W_ci, mode="fan_in", nonlinearity="leaky_relu")
+            nn.init.kaiming_normal_(self.W_co, mode="fan_in", nonlinearity="leaky_relu")
+            nn.init.kaiming_normal_(self.W_cf, mode="fan_in", nonlinearity="leaky_relu")
+            return
 
-            combined_2 = torch.cat((x, r * htprev),
-                                   1)  # h' = tanh(W*(x+r*H_t-1))
-            ht = self.conv2(combined_2)
-            ht = torch.tanh(ht)
-            htnext = (1 - z) * htprev + z * ht
-            output_inner.append(htnext)
-            htprev = htnext
-        return torch.stack(output_inner), htnext
-
-
-class ConvLSTM_cell(nn.Module):
-    """ConvLSTMCell
-    """
-
-    def __init__(self, shape, input_channels, filter_size, num_features):
-        super(ConvLSTM_cell, self).__init__()
-
-        self.shape = shape  # H, W
-        self.input_channels = input_channels
-        self.filter_size = filter_size
-        self.num_features = num_features
-        # in this way the output has the same size
-        self.padding = (filter_size - 1) // 2
-        self.conv = nn.Sequential(
-            nn.Conv2d(self.input_channels + self.num_features,
-                      4 * self.num_features, self.filter_size, 1,
-                      self.padding),
-            nn.GroupNorm(4 * self.num_features // 32, 4 * self.num_features))
-
-    def forward(self, inputs=None, hidden_state=None, seq_len=10):
-        #  seq_len=10 for moving_mnist
-        if hidden_state is None:
-            hx = torch.zeros(inputs.size(1), self.num_features, self.shape[0],
-                             self.shape[1]).cuda()
-            cx = torch.zeros(inputs.size(1), self.num_features, self.shape[0],
-                             self.shape[1]).cuda()
+        elif initializer == WeightsInitializer.Xavier:
+            nn.init.xavier_normal_(self.W_ci, gain=1.0)
+            nn.init.xavier_normal_(self.W_co, gain=1.0)
+            nn.init.xavier_normal_(self.W_cf, gain=1.0)
+            return
         else:
-            hx, cx = hidden_state
-        output_inner = []
-        for index in range(seq_len):
-            if inputs is None:
-                x = torch.zeros(hx.size(0), self.input_channels, self.shape[0],
-                                self.shape[1]).cuda()
-            else:
-                x = inputs[index, ...]
+            raise ValueError(f"Invalid weights Initializer: {initializer}")
 
-            combined = torch.cat((x, hx), 1)
-            gates = self.conv(combined)  # gates: S, num_features*4, H, W
-            # it should return 4 tensors: i,f,g,o
-            ingate, forgetgate, cellgate, outgate = torch.split(
-                gates, self.num_features, dim=1)
-            ingate = torch.sigmoid(ingate)
-            forgetgate = torch.sigmoid(forgetgate)
-            cellgate = torch.tanh(cellgate)
-            outgate = torch.sigmoid(outgate)
+    def forward(
+        self, X: torch.Tensor, prev_h: torch.Tensor, prev_cell: torch.Tensor
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+        new_h, new_cell = self.convlstm_cell(X, prev_h, prev_cell)
+        return new_h, new_cell
 
-            cy = (forgetgate * cx) + (ingate * cellgate)
-            hy = outgate * torch.tanh(cy)
-            output_inner.append(hy)
-            hx = hy
-            cx = cy
-        return torch.stack(output_inner), (hy, cy)
+    def convlstm_cell(
+        self, X: torch.Tensor, prev_h: torch.Tensor, prev_cell: torch.Tensor
+    ):
+        """ConvLSTM cell calculation.
 
+        Args:
+            X (torch.Tensor): input data.
+            h_prev (torch.Tensor): previous hidden state.
+            c_prev (torch.Tensor): previous cell state.
 
-def process_inputs(inputs, subnet):
-    seq_number, batch_size, input_channel, height, width = inputs.size()
-    inputs = torch.reshape(inputs, (-1, input_channel, height, width))
-    inputs = subnet(inputs)
-    inputs = torch.reshape(inputs, (seq_number, batch_size, inputs.size(1),
-                                    inputs.size(2), inputs.size(3)))
-    return inputs
+        Returns:
+            Tuple[torch.Tensor, torch.Tensor]: (current_hidden_state, current_cell_state)
+        """
+        conv_output = self.conv(torch.cat([X, prev_h], dim=1))
 
+        i_conv, f_conv, c_conv, o_conv = torch.chunk(conv_output, chunks=4, dim=1)
 
-def forward_by_stage_encoder(inputs, subnet, rnn):
-    processed_inputs = process_inputs(inputs, subnet)
-    outputs_stage, state_stage = rnn(processed_inputs, None)
-    return outputs_stage, state_stage
+        input_gate = torch.sigmoid(i_conv + self.W_ci * prev_cell)
+        forget_gate = torch.sigmoid(f_conv + self.W_cf * prev_cell)
 
+        # Current cell output (state)
+        C = forget_gate * prev_cell + input_gate * self.activation(c_conv)
 
-class Encoder(nn.Module):
-    def __init__(self, subnets, rnns):
-        super().__init__()
-        assert len(subnets) == len(rnns)
-        self.blocks = len(subnets)
+        output_gate = torch.sigmoid(o_conv + self.W_co * C)
 
-        for index, (params, rnn) in enumerate(zip(subnets, rnns), 1):
-            # index sign from 1
-            setattr(self, 'stage' + str(index), make_layers(params))
-            setattr(self, 'rnn' + str(index), rnn)
+        # Current hidden state
+        H = output_gate * self.activation(C)
 
-    def forward(self, inputs):
-        inputs = inputs.transpose(0, 1)  # to S,B,1,64,64
-        hidden_states = []
-        logging.debug(inputs.size())
-        for i in range(1, self.blocks + 1):
-            inputs, state_stage = forward_by_stage_encoder(
-                inputs, getattr(self, 'stage' + str(i)),
-                getattr(self, 'rnn' + str(i)))
-            hidden_states.append(state_stage)
-        return tuple(hidden_states)
-
-
-def forward_by_stage_decoder(inputs, state, subnet, rnn):
-    inputs, state_stage = rnn(inputs, state, seq_len=10)
-    inputs = process_inputs(inputs, subnet)
-    return inputs
-
-    # input: 5D S*B*C*H*W
-
-
-class Decoder(nn.Module):
-    def __init__(self, subnets, rnns):
-        super().__init__()
-        assert len(subnets) == len(rnns)
-
-        self.blocks = len(subnets)
-
-        for index, (params, rnn) in enumerate(zip(subnets, rnns)):
-            setattr(self, 'rnn' + str(self.blocks - index), rnn)
-            setattr(self, 'stage' + str(self.blocks - index),
-                    make_layers(params))
-
-    def forward(self, hidden_states):
-        inputs = forward_by_stage_decoder(None, hidden_states[-1],
-                                          getattr(self, 'stage3'),
-                                          getattr(self, 'rnn3'))
-        for i in list(range(1, self.blocks))[::-1]:
-            inputs = forward_by_stage_decoder(inputs, hidden_states[i - 1],
-                                              getattr(self, 'stage' + str(i)),
-                                              getattr(self, 'rnn' + str(i)))
-        inputs = inputs.transpose(0, 1)  # to B,S,1,64,64
-        return inputs
-
-
-class activation:
-
-    def __init__(self, act_type, negative_slope=0.2, inplace=True):
-        super().__init__()
-        self._act_type = act_type
-        self.negative_slope = negative_slope
-        self.inplace = inplace
-
-    def __call__(self, input):
-        if self._act_type == 'leaky':
-            return F.leaky_relu(input, negative_slope=self.negative_slope, inplace=self.inplace)
-        elif self._act_type == 'relu':
-            return F.relu(input, inplace=self.inplace)
-        elif self._act_type == 'sigmoid':
-            return torch.sigmoid(input)
-        else:
-            raise NotImplementedError
-
-
-class EncoderDecoder(TimeSeriesModel):
-
-    def __init__(self, encoder, decoder):
-        super().__init__()
-        self.encoder = encoder
-        self.decoder = decoder
-
-    def forward(self, input):
-        state = self.encoder(input)
-        output = self.decoder(state)
-        return output
-
-
+        return H.to(DEVICE), C.to(DEVICE)
 
 
